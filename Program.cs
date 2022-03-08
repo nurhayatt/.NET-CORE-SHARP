3b87bf9f443a0d555d156df6f2e38e3324af359e
nurhayatt <72298952+nurhayatt@users.noreply.github.com>
2022-03-08 14:26:16 +0300

    Değişkenler

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace consoleapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            //Kilo bilgisini tutacak bir değişken(byte, short, int, long)
+            byte kilo = 100;
+            byte? kilo2 = null;
+            //Plaka bilgisini tutacak bir değişken
+            byte plaka = 81;
+            string _plaka = "81";
+            //Araç km bilgisini tutan bir değişken
+            int km = 100000;
+            //Müşteri id si tutacak bir değişken
+            int müsteriId = 1;
+            string musteri_id= "A1111";   
+            //Bir ürünün satuşta olup olmadığunı tutuan bilgisi
+            bool satışta = true;    
+            //Maaş bilgisini tutacak değişken(float, double, decimal)
+            float maas = 100000.00f;
+            double maas2 = 100000.00;
+            decimal maas3 = 100000.00m;
+            //Öğrenci adını tutacak bir değişken
+                string ad = "Nur";
+                string soyad = "Berber";
+                string adSoyad = ad + " " + soyad;  //Nur Berber
+
+
+
         }
     }
 }
